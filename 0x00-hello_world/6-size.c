5ab809af7a8cba2b9d0fc93027fb1cf4be772291
ahmad kadri <122832960+akdry@users.noreply.github.com>
2023-02-17 01:53:17 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Description: 'the program's description'
- * 
- * Return: Always 0 (Success)
+ * printf: Prints sizes of characters
+ * Return:0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)  sizeof(b));
