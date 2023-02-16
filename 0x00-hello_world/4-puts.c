f673864dd4d90fc8e81a7302a87975a9687957c8
ahmad kadri <122832960+akdry@users.noreply.github.com>
2023-02-17 00:06:51 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts functio
+ n
+ * Return: 0 (Success)
  */
 int main(void)
 {
