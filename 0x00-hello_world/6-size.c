4660c365235885d1ce7b7bc364bb61e30338ffa7
akdry <a.2dry11@gmail.com>
2023-02-16 16:13:22 -0800

    yayayay

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <stdint.h>
 
 /**
  * main - Entry point
- * printf: Prints sizes of characters
- * Return: 0
+ *
+ * Description: Prints sizes of characters
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     char a;
@@ -19,17 +21,6 @@ int main(void)
     printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
     printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
     printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-    /* check if machine is 32-bit or 64-bit */
-    if (sizeof(void *) == 4)
-    {
-        uint32_t ptr_size = sizeof(void *);
-        printf("Size of a pointer: %u byte(s)\n", ptr_size);
-    }
-    else
-    {
-        printf("Size of a pointer: %lu byte(s)\n", (unsigned long)sizeof(void *));
-    }
 
     return (0);
 }
